06429138ac3bf04cdf6daefe5a4933d2b3cce946
Nicole Gatmaitan <nicolegatmaitan@Nicoles-MacBook-Pro.local>
2018-09-30 10:34:25 -0500

    made great strides in the website today. figured out browsersync with gulp, added main content. a little bit more content to add and then to make it interesting

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-var nunjucksRender = require('gulp-nunjucks-render');
+var gulp = require('gulp'),
+	nunjucksRender = require('gulp-nunjucks-render'),
+	less = require('gulp-less'),
+	minifyCSS = require('gulp-csso'),
+	//minifyJS = require('gulp-minify'),
+	browserSync = require('browser-sync').create();
 
 gulp.task('nunjucks', function() {
 	// Gets .html and .nunjucks files in pages
@@ -10,4 +14,40 @@ gulp.task('nunjucks', function() {
  	}))
 	// output files in app folder
 	.pipe(gulp.dest('app'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
 });
+
+gulp.task('css', function(){
+	return gulp.src('app/less/*.less')
+	.pipe(less())
+	.pipe(minifyCSS())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
+});
+
+/*gulp.task('js', function(){
+	return gulp.src('app/js/*.js')
+	.pipe(minify())
+	.pipe(gulp.dest('app'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
+});*/
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: "app"
+	})
+})
+
+gulp.task('watch', ['browserSync', 'css', 'nunjucks'], function(){
+	gulp.watch("app/less/*.less", ['css']);
+	//sgulp.watch("app/js/*.js", ['js']);
+	gulp.watch("app/templates/partials/*.nunjucks", ['nunjucks']);
+});
+
+//gulp.task('default', ['nunjucks','css','watch']);
