8949074948c6adcbcdc40f571cd5e03dd136f98d
ngatmaitan <ngatmaitan@gmail.com>
2018-11-03 00:58:38 -0500

    added posters for volunteer work, optimized images. only responsive behavior left

--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,18 @@ $('.skillbar').each( function() {
 	$(this).find('.skillbar-bar').css('width', $(this).attr('data-percent'));
 	$(this).find('.skillbar-percent').text($(this).attr('data-percent'));
 })
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+}); 
+
+$('#poster-gallery').showMoreImages({
+	minheight: 0
+});  
--- app/scripts.js
+++ app/scripts.js
@@ -63,6 +63,70 @@
 
 }(jQuery));
 
+(function ( $ ) {
+    
+    /*
+    Plugin: ShowMore
+    author: dtasic@gmail.com
+    */
+    
+    $.fn.showMoreImages = function (options) {
+        
+        "use strict";
+        
+        var currentelem = 1;
+        
+        this.each(function(){
+            
+            var currentid = '';
+            var element = $(this);
+            var auto = parseInt(element.innerHeight())/2;
+            var fullheight = element.innerHeight();
+            var maxWidth = element.css('width');
+            var settings = $.extend({
+                minheight: auto,
+                buttontxtmore: "<i class=\"fas fa-search-plus vw-icon\"></i>",
+                buttontxtless: "<i class=\"fas fa-search-minus vw-icon\"></i>",
+                buttoncss: "showmore-button",
+                animationspeed: auto       
+            }, options );        
+            
+            element.attr('id') != undefined ? currentid = element.attr('id') : currentid = currentelem;
+            element.wrap( "<div id='showmore-"+currentid+"' data-showmore"+(maxWidth == '0px' ? "" : " style='max-width:"+maxWidth+";'")+"></div>" );
+            if (element.parent().not('[data-showmore]')) {
+
+                if (fullheight > settings.minheight) {
+                    
+                    element.css('min-height', settings.minheight).css('max-height', settings.minheight).css('overflow', 'hidden');
+                    var showMoreButton = $("<div />", {
+                        id: "showmore-button-"+currentid,
+                        "class": settings.buttoncss,
+                        click: function() {
+
+                            if (element.css('max-height') != 'none') {
+                                element.css('height', settings.minheight).css('max-height', '').animate({height:fullheight}, settings.animationspeed, function () { showMoreButton.html(settings.buttontxtless); });
+                            } else {
+                                element.animate({height:settings.minheight}, settings.animationspeed, function () { showMoreButton.html(settings.buttontxtmore); element.css('max-height', settings.minheight); });
+                            }
+                        },
+                        html: settings.buttontxtmore
+                    });
+
+                    element.after(showMoreButton);
+
+                }
+                
+                currentelem++;
+                
+            }
+            
+        });
+        
+        return this;
+        
+    };
+
+}(jQuery));
 var date = new Date();
 var currentYear = date.getFullYear();
 $('footer #footer-text #copyright #copyright-year').text(currentYear);
@@ -75,3 +139,18 @@ $('.skillbar').each( function() {
 	$(this).find('.skillbar-bar').css('width', $(this).attr('data-percent'));
 	$(this).find('.skillbar-percent').text($(this).attr('data-percent'));
 })
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+}); 
+
+$('#poster-gallery').showMoreImages({
+	minheight: 0
+});  
